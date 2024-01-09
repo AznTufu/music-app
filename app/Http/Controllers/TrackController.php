0f23137be70e4739fc9c3e24b52a917de6f6688d
Tony Zhang <zhgtony.pro@hotmail.com>
2024-01-09 13:49:34 +0100

    add Form & store

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use inertia\Inertia;
 use App\Models\Track;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TrackController extends Controller
 {
@@ -19,5 +22,25 @@ class TrackController extends Controller
     public function create()
     {
         return inertia::render('Track/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['string', 'required', 'max:255'],
+            'artist' => ['string', 'required', 'max:255'],
+            'display' => ['boolean', 'required'],
+            'image' => ['image', 'required'],
+            'music' => ['file', 'required', 'extensions:mp3,wav'],
+        ]);
+        
+        Track::create([
+            'uuid' => 'trk-' . Str::uuid(), // 'trk-uuid
+            'title' => $request->title,
+            'artist' => $request->artist,
+            'display' => $request->display,
+            'image' => $request->image->store('images', 'public'),
+            'music' => $request->music->store('music', 'public'),
+        ]);
     }
 }
--- resources/js/Pages/Track/Create.vue
+++ resources/js/Pages/Track/Create.vue
@@ -11,7 +11,89 @@
       </Link>
     </template>
 
-    <template #content> </template>
+    <template #content>
+      <form @submit.prevent="submit">
+        <div class="mb-3">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+            Titre
+          </label>
+          <input
+              id="title"
+              v-model="form.title"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              :class="{ 'border-red-500': form.errors.title }"
+              type="text"
+              placeholder="Title"
+          >
+          <p class="text-red-500 text-xs italic"> {{ form.errors.title }}</p>
+        </div>
+
+        <div class="mb-3">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="artist">
+            Artiste
+          </label>
+          <input
+              id="artist"
+              v-model="form.artist"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              :class="{ 'border-red-500': form.errors.artist }"
+              type="text"
+              placeholder="Artist"
+          >
+          <p class="text-red-500 text-xs italic"> {{ form.errors.artist }}</p>
+        </div>
+
+        <div class="mb-3">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="image">
+            Miniature
+          </label>
+          <input
+              id="image"
+              @input="form.image = $event.target.files[0]"
+              :class="{ 'border-red-500': form.errors.image }"
+              type="file"
+          >
+          <p class="text-red-500 text-xs italic"> {{ form.errors.image }}</p>
+        </div>
+
+        <div class="mb-3">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="music">
+            Musique
+          </label>
+          <input
+              id="music"
+              @input="form.music = $event.target.files[0]"
+              :class="{ 'border-red-500': form.errors.music }"
+              type="file"
+          >
+          <p class="text-red-500 text-xs italic"> {{ form.errors.music }}</p>
+        </div>
+        
+        <div class="mb-3">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="display">
+            Afficher
+          </label>
+          <select 
+            id="display"
+            v-model="form.display"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            :class="{ 'border-red-500': form.errors.display }"
+            type="select"
+          >
+            <option :value="true">Oui</option>
+            <option :value="false">Non</option>
+          </select>
+          <p class="text-red-500 text-xs italic"> {{ form.errors.display }}</p>
+        </div>
+
+        <input 
+          type="submit" 
+          value="Créer la musique"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
+        >
+      </form>
+      {{ form.errors }}
+    </template>
   </MusicLayout>
 </template>
 
@@ -23,6 +105,22 @@ export default {
   components: {
     MusicLayout,
     Link,
+  },
+  data() {
+    return {
+      form: this.$inertia.form({
+        title: "",
+        artist: "",
+        image: null,
+        music: null,
+        display: true,
+      }),
+    };
+  },
+  methods: {
+    submit() {
+      this.form.post(route("tracks.store"));
+    },
   },
 };
 </script>
